3c0b8aa584ce61060ab85d9acd0d8d0adf8f0f70
AaronGavendo <agavendo@hotmail.com>
2016-06-11 22:47:06 -0400

    #3 Add label, textbox and button for the Name=Value box. Set up the foundation for the jQuery to handle the data.

--- dundas.js
+++ dundas.js
@@ -1,1 +1,17 @@
 console.log("Test JavaScript");
+
+//Function to run when website is loaded
+$(document).ready(function () {
+console.log("Ready Function Loads");
+
+
+
+	//When the Add button is pressed
+	$("#add").click(function () {
+		console.log("Add button has been pressed");
+
+	});
+
+});
+
+
